f1e9ef2ab803c287abd52ebb633252a9a66de518
Cecilia Møller <cecilia@Cecilias-MacBook-Air.local>
2022-07-05 12:55:55 +0100

    First commit

--- src/Questions.js
+++ src/Questions.js
@@ -1,18 +1,505 @@
+
 const questions = [
   {
-    header: "Pekelek",
-    content: "Hvem er penest?",
-  },
-  {
-    header: "Pekelek",
-    content: "Hvem er lavest?",
-    description: "Drikk antall pek",
-  },
-
-  {
-    header: "Pekelek",
-    content: "Hvem er penest?",
-  },
+    header: "", 
+    value: "1", 
+    content:"Alle fra omega kan dele ut en slurk til en fra smørekoppen",
+   }, 
+   {
+    header: "", 
+    value: "1", 
+    content:"Alle fra smørekoppen kan dele ut en slurk til en fra omega",
+   }, 
+   {
+    header: "", 
+    value: "5", 
+    content:"Ta lambo på alle som drikker Dahls",
+   }, 
+   {
+    header: "Drikk hvis", 
+    value: "1", 
+    content:"Drikk hvis du liker betong",
+   }, 
+   {
+    header: "LAMBO!!", 
+    value: "1", 
+    content:"",
+   }, 
+   {
+    header: "LAMBO!!", 
+    value: "1", 
+    content:"",
+   }, 
+   {
+    header: "LAMBO!!", 
+    value: "1", 
+    content:"",
+   }, 
+   {
+    header: "LAMBO!!", 
+    value: "1", 
+    content:"",
+   }, 
+   {
+    header: "LAMBO!!", 
+    value: "1", 
+    content:"",
+   }, 
+   {
+    header: "Drikk hvis", 
+    value: "1", 
+    content:"Drikk hvis du har hatt Skau",
+   }, 
+   {
+    header: "Drikk hvis", 
+    value: "1", 
+    content:"Drikk hvis du har A-snitt",
+   }, 
+   {
+    header: "Drikk hvis", 
+    value: "1", 
+    content:"Drikk hvis du tror du er blant de 5 mest attraktive i rommet",
+   }, 
+   {
+    header: "Drikk hvis", 
+    value: "1", 
+    content:"Drikk hvis du tror du er blant de 5 smarteste i rommet",
+   }, 
+   {
+    header: "Drikk hvis", 
+    value: "2", 
+    content:"Alle «hetero» indøkere må ta en shot",
+   }, 
+   {
+    header: "Drikk hvis", 
+    value: "3", 
+    content:"Drikk hvis du er keen på en i rommet",
+   }, 
+   {
+    header: "Drikk hvis", 
+    value: "4", 
+    content:"Drikk hvis du er ny-shava",
+   }, 
+   {
+    header: "Drikk hvis", 
+    value: "5", 
+    content:"Drikk hvis du har pult noen i rommet",
+   }, 
+   {
+    header: "Drikk hvis", 
+    value: "5", 
+    content:"Drikk hvis du skal pule en i rommet",
+   }, 
+   {
+    header: "Drikk hvis", 
+    value: "6", 
+    content:"Drikk hvis du har pult de siste 24 timene",
+   }, 
+   {
+    header: "Drikk hvis", 
+    value: "7", 
+    content:"Drikk hvis du har pult noen av samme kjønn",
+   }, 
+   {
+    header: "Drikk hvis", 
+    value: "8", 
+    content:"Drikk hvis du har/har blitt rimma",
+   }, 
+   {
+    header: "Drikk hvis", 
+    value: "8", 
+    content:"Drikk hvis du har hatt trekant",
+   }, 
+   {
+    header: "Drikk hvis", 
+    value: "8", 
+    content:"Drikk hvis du har hatt anal",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "1", 
+    content:"Jeg har aldri vært på pendelnasj",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "1", 
+    content:"Jeg har aldri tatt en taktisk kont",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "1", 
+    content:"Jeg har aldri latet som at en kont var taktisk",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "2", 
+    content:"Jeg har aldri stjålet et flagg",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "2", 
+    content:"Jeg har aldri løyet om hva jeg studerer på fylla",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "2", 
+    content:"Jeg har aldri vært på DT-torsdag",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "2", 
+    content:"Jeg har aldri crusha på en professor",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "2", 
+    content:"Jeg har aldri blitt avvist på byen",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "2", 
+    content:"Jeg har aldri brukt fake leg",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "3", 
+    content:"Jeg har aldri blitt drita på gløs",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "3", 
+    content:"Jeg har aldri vært på samf til stengetid",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "3", 
+    content:"Jeg har aldri tilbrakt en hel kveld i bodegaen",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "3", 
+    content:"Jeg har aldri dumpet noen",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "3", 
+    content:"Jeg har aldri blitt dumpa",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "3", 
+    content:"Jeg ha aldri blacka ut",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "4", 
+    content:"Jeg har aldri måttet chugge til lambo",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "4", 
+    content:"Jeg har aldri pult en studass",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "4", 
+    content:"Jeg har aldri løyet for å få meg noe",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "4", 
+    content:"Jeg har aldri pult en fadder",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "4", 
+    content:"Jeg har aldri pult et fadderbarn",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "4", 
+    content:"Jeg har aldri pult på gløs",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "4", 
+    content:"Jeg har aldri pult en fra BI",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "4", 
+    content:"Jeg har aldri funnet ligg på bodegaen",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "4", 
+    content:"Jeg har aldri blitt glad for å få mensen",
+   }, 
+   {
+    header: "Jeg har aldri", 
+    value: "4", 
+    content:"Jeg har aldri kjørt brannbil",
+   }, 
+   {
+    header: "Jeg har aldri ", 
+    value: "3", 
+    content:"Jeg ha aldri vært drita med familien",
+   }, 
+   {
+    header: "Jeg har aldri ", 
+    value: "3", 
+    content:"jeg har aldri havnet på legevakten på fylla",
+   }, 
+   {
+    header: "Jeg har aldri ", 
+    value: "4", 
+    content:"Jeg har aldri hatt en penis",
+   }, 
+   {
+    header: "Jeg har aldri ", 
+    value: "4", 
+    content:"Jeg har aldri sett prono",
+   }, 
+   {
+    header: "Jeg har aldri ", 
+    value: "5", 
+    content:"Jeg har aldri pult noen med en kjønnssykdom ",
+   }, 
+   {
+    header: "Jeg har aldri ", 
+    value: "5", 
+    content:"Jeg har aldri fått noe for å ligge med noen",
+   }, 
+   {
+    header: "Jeg har aldri ", 
+    value: "6", 
+    content:"Jeg har aldri røyket hasj",
+   }, 
+   {
+    header: "Jeg har aldri ", 
+    value: "6", 
+    content:"Jeg har aldri onanert på gløs",
+   }, 
+   {
+    header: "Jeg har aldri ", 
+    value: "6", 
+    content:"Jeg har aldri hatt sex med bind for øynene",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "1", 
+    content:"Person a, hvem er mest attraktiv i rommet?",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "1", 
+    content:"Person a kan velge seg en drikkepartner",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "1", 
+    content:"Person a kan dele ut 4 slurker til den som stråler mest",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "1", 
+    content:"Person a kan dele ut 4 slurker til den peneste i rommet",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "1", 
+    content:"Person a kan dele ut 4 slurker til den som lukter best",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "2", 
+    content:"Person a og person b må ha øyekontakt de neste 3 spørsmålene",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "4", 
+    content:"Drikk en slurk for hver pulbare person  i rommet",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "4", 
+    content:"Person a må si sin største fetish, eller ta 4 slurker ",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "4", 
+    content:"Person a må chugge drikken sin",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "4", 
+    content:"Person a må sitte på fanget til person  b de neste 3 spørsmålene",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "4", 
+    content:"Person a kan dele ut 2 slurker til den som ser mest på porno",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "5", 
+    content:"Person a og person b må bytte drikke ",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "6", 
+    content:"Person a kan dele ut 2 slurker til den som er mest kinky",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "8", 
+    content:"Person a: Fuck marry kill, person b, c og d.",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "9", 
+    content:"Person a må ta av tre klesplagg, og ha dem av de neste 5 rundene",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "9", 
+    content:"Person a skal ta av seg buksene, og kan ta dem på etter minst 3 runder. ",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "9", 
+    content:"Person a må hooke noen i rommet",
+   }, 
+   {
+    header: "Navn-spesifikt", 
+    value: "10", 
+    content:"Person a velger 3 plakk person b skal ta av seg. Person b kan kle på seg igjen etter 2 runder. ",
+   }, 
+   {
+    header: "Ok, red flagg eller dealbreaker:", 
+    value: "1", 
+    content:"Personen studerer på Dragvoll",
+   }, 
+   {
+    header: "Ok, red flagg eller dealbreaker:", 
+    value: "1", 
+    content:"Personen går fysmat",
+   }, 
+   {
+    header: "Ok, red flagg eller dealbreaker:", 
+    value: "1", 
+    content:"Personen er fra trondheim ",
+   }, 
+   {
+    header: "Ok, red flagg eller dealbreaker:", 
+    value: "1", 
+    content:"Personen bor på Moholt",
+   }, 
+   {
+    header: "Ok, red flagg eller dealbreaker:", 
+    value: "2", 
+    content:"Bodegaen er favorittstedet hans/hennes på Samf",
+   }, 
+   {
+    header: "Ok, red flagg eller dealbreaker:", 
+    value: "2", 
+    content:"Personen liker å stjele ting på fylla ",
+   }, 
+   {
+    header: "Pekelek", 
+    value: "1", 
+    content:"Hvem har størst baller?",
+   }, 
+   {
+    header: "Pekelek", 
+    value: "1", 
+    content:"Hvem er mest fronøyd med eget utseende?",
+   }, 
+   {
+    header: "Pekelek", 
+    value: "1", 
+    content:"Hvem har best kropp?",
+   }, 
+   {
+    header: "Pekelek", 
+    value: "1", 
+    content:"Hvem har finest smil?",
+   }, 
+   {
+    header: "Pekelek", 
+    value: "2", 
+    content:"Hvem har den sjukeste historien?",
+   }, 
+   {
+    header: "Pekelek", 
+    value: "3", 
+    content:"Hvem er den beste kysseren",
+   }, 
+   {
+    header: "Pekelek", 
+    value: "3", 
+    content:"Hvem ender på st. Olavs i kveld?",
+   }, 
+   {
+    header: "Pekelek", 
+    value: "4", 
+    content:"Hvem er kåtest?",
+   }, 
+   {
+    header: "Pekelek", 
+    value: "4", 
+    content:"Hvem har hatt sex med flest i dette rommet",
+   }, 
+   {
+    header: "Pekelek", 
+    value: "5", 
+    content:"Hvem er best i senga?",
+   }, 
+   {
+    header: "Pekelek", 
+    value: "5", 
+    content:"Hvem har tatt flest angrepiller?",
+   }, 
+   {
+    header: "Pekelek", 
+    value: "9", 
+    content:"Hvem ville gitt den beste lap dancen?",
+   }, 
+   {
+    header: "Regel", 
+    value: "1", 
+    content:"Ikke lov til å drikke med høyre hånd",
+   }, 
+   {
+    header: "Regel", 
+    value: "1", 
+    content:"Ikke lov til å bruke fornavn",
+   }, 
+   {
+    header: "Regel", 
+    value: "1", 
+    content:"Ikke lov til å peke ved hjelp av armene",
+   },
 ];
+ 
+function shuffle(array) { //hentet fra: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  let currentIndex = array.length,  randomIndex;
 
-export default questions;
+  // While there remain elements to shuffle.
+  while (currentIndex !== 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+
+export default shuffle(questions);
+
