125debf89fdf95ded70c3bcc0095534beb6b36cf
Ceciliamoller <ceciliamollerblom@gmail.com>
2022-08-12 15:04:59 +0200

    small fix

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -9,11 +9,7 @@ import Question from "../Components/Question";
 import Addmembers from "../Components/Addmembers";
 import Droyhetsskala from "./Droyhetsskala";
 
-<<<<<<< HEAD
 import StartsideNidarus from '../Assets/NidarusStart.gif';
-=======
-import StartsideNidarus from '../Assets/NidarusStart2.gif';
->>>>>>> d9e98d13a94f287b2bcc90ae01b1154940a30fbf
 import TittelNidarus from '../Assets/TittelNidarus.PNG';
 import EmailButton from "./Emailbutton";
 
@@ -45,7 +41,6 @@ function MainPage() {
                     <>  <Question /> </> :
                     started ?
                         <VStack h='100%' w="100%" spacing='20px' >
-<<<<<<< HEAD
                             <Box mt="150px"  >
                                 <Droyhetsskala myColor='white' > </Droyhetsskala>
                             </Box>
@@ -55,12 +50,6 @@ function MainPage() {
                                 <Text as='kbd' ml='90px' fontSize="xl" color="white"> Gløs-tema: </Text>
                                 <Switch colorScheme="red" id='glostema' defaultChecked="on" onChange={() => glostema = !glostema}> </Switch>
                             </HStack>
-=======
-                            <Box mt="240px" bg="#f3a040" >
-                                <Droyhetsskala myColor='white' > </Droyhetsskala>
-                            </Box>
-                            <Button onClick={() => setFinalStarted(true)}> Lagre</Button>
->>>>>>> d9e98d13a94f287b2bcc90ae01b1154940a30fbf
                         </VStack> :
                         <Box>
                             <VStack
@@ -79,11 +68,7 @@ function MainPage() {
                                     right="0"
                                     bottom="400"
                                 />
-<<<<<<< HEAD
                                 <Button as="kbd" color="#004e00" onClick={() => setStarted(true)}>
-=======
-                                <Button onClick={() => setStarted(true)}>
->>>>>>> d9e98d13a94f287b2bcc90ae01b1154940a30fbf
                                     Start
                                 </Button>
                                 <Addmembers></Addmembers>
